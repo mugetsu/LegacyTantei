a8c5a865c99e82336ecf6a92acada9e7b60bca29
Randell Quitain <cprjk.buzz@gmail.com>
2022-09-28 23:43:41 +0800

    Initial Commit

---
+++ Tantei-san/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tantei-san
+//
+//  Created by Randell on 28/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
