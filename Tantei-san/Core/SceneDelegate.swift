d81f155559c7566b23f23e95e2fe92ac2d3c74f6
Randell Quitain <cprjk.buzz@gmail.com>
2022-09-29 08:15:30 +0800

    chore: Use Splash as default root view

--- Tantei-san/Core/SceneDelegate.swift
+++ Tantei-san/Core/SceneDelegate.swift
@@ -16,11 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let viewModel = SearchViewModel()
-        let viewController = SearchViewController(viewModel: viewModel)
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = viewController
+        window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
     }
 
